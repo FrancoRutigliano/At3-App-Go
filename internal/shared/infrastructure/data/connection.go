b331280a5d4381daf09ae797dcf1f65edc9fd90f
Franco Rutigliano <francorutigliano11@gmail.com>
2024-10-23 10:53:33 -0300

    connection to db

--- internal/shared/infrastructure/data/connection.go
+++ internal/shared/infrastructure/data/connection.go
@@ -1,5 +1,17 @@
 package data
 
-func GetConnection() {
+import (
+	"os"
 
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func GetConnection() (*sqlx.DB, error) {
+
+	db, err := sqlx.Connect("postgres", os.Getenv("CONNECTION"))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
